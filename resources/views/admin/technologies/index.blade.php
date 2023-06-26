a7018f8fec603064d88ea38feda2109a335ea73e
raffaele-catalano <raf.catalano@gmail.com>
2023-06-26 17:48:11 +0200

    minor graphic enhancements

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
     <h3 class="text-center my-3">Project Technologies</h3>
-    <div class="container d-flex flex-column align-items-center">
-        <table class="table">
+    <div class="container d-flex flex-column align-items-center w-50">
+        <table class="table table-dark">
             <thead>
                 <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Projects Involved</th>
+                    <th scope="col-4">Name</th>
+                    <th class="text-center" scope="col-6">Number of Projects Including These Technologies</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ( $technologies as $technology )
                 <tr>
                     <td>{{ $technology->name }}</td>
-                    <td>{{ count($technology->projects) }}</td>
+                    <td class="text-center">{{ count($technology->projects) }}</td>
                 </tr>
                 @endforeach
             </tbody>
--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
     <h3 class="text-center my-3">Project Types</h3>
-    <div class="container d-flex flex-column align-items-center">
-        <table class="table">
+    <div class="container d-flex flex-column align-items-center w-50">
+        <table class="table table-dark">
             <thead>
                 <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Projects Involved</th>
+                    <th scope="col-3">Name</th>
+                    <th class="text-center" scope="col-4">Number of Projects of this Type</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ( $types as $type )
                 <tr>
                     <td>{{ $type->name }}</td>
-                    <td>{{ count($type->projects) }}</td>
+                    <td class="text-center">{{ count($type->projects) }}</td>
                 </tr>
                 @endforeach
             </tbody>
