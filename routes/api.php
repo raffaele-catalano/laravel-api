30da9502f86b9c7aace27543281ff0df48776183
raffaele-catalano <raf.catalano@gmail.com>
2023-06-26 17:20:58 +0200

    implemented a new Route for Api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::namespace('Api')
+        ->prefix('projects')
+        ->group(function(){
+            Route::get('/', [ProjectController::class, 'index']);
+        });
