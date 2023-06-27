0e591c08062c1755a7258c2c946d02fc11c6c2e9
raffaele-catalano <raf.catalano@gmail.com>
2023-06-27 17:02:50 +0200

    fixed important Query in Api/ProjectController + fix

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,8 @@ class ProjectController extends Controller
 {
     public function index() {
 
-        $projects = Project::all();
+        // $projects = Project::all();
+        $projects = Project::with('type', 'technologies')->get();
 
         return response()->json($projects);
     }
