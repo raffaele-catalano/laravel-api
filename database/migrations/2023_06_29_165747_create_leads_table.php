1289ae4419e0dd5f0f8fb96c8e59c8c8ee152aa1
raffaele-catalano <raf.catalano@gmail.com>
2023-06-29 21:08:23 +0200

    changed message from string to text

--- database/migrations/2023_06_29_165747_create_leads_table.php
+++ database/migrations/2023_06_29_165747_create_leads_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
-            $table->string('message');
+            $table->text('message');
             $table->timestamps();
         });
     }
